8d26eb7e83feb21143de885bc7cae8e4b3d9a2c6
YusraSenimDede <50294199+YusraSenimDede@users.noreply.github.com>
2021-06-29 17:58:44 +0300

    not con db

--- db/connetDatabase.js
+++ db/connetDatabase.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+
+
+MONGO_URI="mongodb+srv://yusradede:senim1234@restapi.umk3d.mongodb.net/restapi?retryWrites=true&w=majority"
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
             useFindAndModify: false,
             useCreateIndex: true,
